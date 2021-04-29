a3bff6075405c03cbe46c4a110c373e1f575cf35
ukmaty <ukmaty@gmail.com>
2021-04-30 05:38:55 +0900

    Update just for checking

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import * as contentful from "contentful";
 import './App.css';
 
 function App() {
   const [works, setWorks] = useState();
+
   const client = contentful.createClient({
     space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
     accessToken: process.env.REACT_APP_CONTENTFUL_CDA_TOKEN 
   });
 
   useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const resp = await client.getEntries({content_type: "works"});
+        setWorks(resp.items);
+      } catch(error) {
+          console.log("error: ", error);
+        }
+    };
       fetchData();
     }, [works]
   );
 
-  const fetchData = useCallback(
-    async() => {
-    try {
-      const resp = await client.getEntries({content_type: "works"});
-      setWorks(resp.items);
-    } catch(error) {
-      console.log("error: ", error);
-    }
-  });
+
+
 
   return (
     <div className="App">
