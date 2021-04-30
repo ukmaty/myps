b87035829fe3e54a16b50935c8cfcb9486c88e98
ukmaty <ukmaty@gmail.com>
2021-04-30 13:19:21 +0900

    Add font setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import FrontPage from "./components/FrontPage";
 import AboutPage from "./components/AboutPage";
 import './App.css';
